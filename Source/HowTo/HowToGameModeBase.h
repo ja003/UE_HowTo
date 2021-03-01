424256b31cd5bac5077b62fff3f01d426d94356c
Adam Jurík <ja004@seznam.cz>
2021-03-01 13:35:03 +0100

    init

---
+++ Source/HowTo/HowToGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HowToGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOWTO_API AHowToGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
